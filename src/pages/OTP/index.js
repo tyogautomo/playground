daff5dcce7ff4c69c1fa10ac5832e4ebe336f812
tyogautomo <yoga.utomo27@gmail.com>
2022-01-27 02:43:11 +0700

    styling success

--- src/pages/OTP/index.js
+++ src/pages/OTP/index.js
@@ -1,19 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-import { Container, Title, InputContainer, Box, Input, Num } from './styles';
+import { Container, Title, InputContainer, Box, Input, Num, Note, NoteContainer, Button, Loading, Form } from './styles';
 
 const OTPPage = () => {
+  const form = useRef(null);
+
   const [otp, setOtp] = useState('');
   const [focused, setFocused] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
 
-  const handleChange = (e) => {
-    setOtp(e.target.value);
-    console.log(e.target.value);
-  };
+  const handleConfirmation = (e) => {
+    e.preventDefault();
+    setSuccess(true);
+  }
 
   useEffect(() => {
     if ('OTPCredential' in window) {
       const ac = new AbortController();
+      form.current.addEventListener('submit', () => {
+        ac.abort();
+      }, false)
+
       navigator.credentials
         .get({
           otp: { transport: ['sms'] },
@@ -22,6 +30,11 @@ const OTPPage = () => {
         .then(otpObj => {
           setOtp(otpObj.code);
           ac.abort();
+
+          setLoading(true);
+          setTimeout(() => {
+            setLoading(false);
+          }, 2000);
         })
         .catch(err => {
           ac.abort();
@@ -34,26 +47,46 @@ const OTPPage = () => {
     }
   }, []);
 
+
   return (
     <Container>
-      <Title>Masukkan Kode OTPe</Title>
-      <div>Kode OTP kamu adalah: {`${otp}`}</div>
-      <InputContainer>
-        {Array(4).fill('').map((_, i) => (
-          <Box key={i} focused={focused} otpLength={otp.length}>
-            <Num>{otp.split('')[i]}</Num>
-          </Box>
-        ))}
-        <Input
-          type="text"
-          maxLength="4"
-          onFocus={() => setFocused(true)}
-          onBlur={() => setFocused(false)}
-          onChange={handleChange}
-          value={otp}
-        />
-      </InputContainer>
-    </Container>
+      {success ? (
+        <Title>Kode OTP Sukses!</Title>
+      ) : (
+        <>
+          <Title>Masukkan Kode OTP</Title>
+          <Form onSubmit={handleConfirmation} ref={form}>
+            <InputContainer>
+              {Array(4).fill('').map((_, i) => (
+                <Box key={i} focused={focused}>
+                  <Num>{otp.split('')[i]}</Num>
+                </Box>
+              ))}
+              <Input
+                type="text"
+                maxLength="4"
+                onFocus={() => setFocused(true)}
+                onBlur={() => setFocused(false)}
+                onChange={(e) => setOtp(e.target.value)}
+                value={otp}
+              />
+            </InputContainer>
+            <Note bold>Cara ngirim OTP</Note>
+            <Note>Kirim SMS dengan format berikut:</Note>
+            <br></br>
+            <NoteContainer>
+              <Note>{`<pesan SMS nya apa aja terserah>`}</Note>
+              <br></br>
+              <Note red>@fancy-otp.web.app #1234</Note>
+            </NoteContainer>
+            <Button type="submit">
+              {!loading && <span>Konfirmasi</span>}
+              {loading && <Loading />}
+            </Button>
+          </Form>
+        </>
+      )}
+    </Container >
   );
 }
 
--- src/pages/OTP/styles.js
+++ src/pages/OTP/styles.js
@@ -1,24 +1,55 @@
 import tw, { styled, css } from 'twin.macro';
 
 export const Container = styled.div`
-  ${tw`flex flex-col items-center padding-top[100px]`};
+  ${tw`flex flex-col items-center padding-top[50px]`};
 `;
-
 export const Title = styled.div`
-  ${tw`font-size[2rem] mb-20 text-purple-400`};
+  ${tw`font-size[2rem] mb-16 font-weight[bold] color[#488c6a]`};
 `;
-
-export const InputContainer = tw.div`flex relative mt-4`;
-
+export const Form = tw.form`flex flex-col items-center`;
+export const InputContainer = tw.div`flex relative mt-4 mb-10`;
 export const Box = styled.div(({ focused, otpLength }) => [
   tw`w-16 h-16 rounded-md flex justify-center items-center mx-1 bg-white`,
   (focused || otpLength === 4) && css`
-    border: 2px solid green;
+    border: 2px solid #488c6a;
   `,
 ]);
-
 export const Num = tw.div`font-size[2.6rem]`;
-
 export const Input = styled.input`
   ${tw`absolute w-full h-full opacity-0`};
+`;
+export const Note = styled.div(({ bold, red }) => [
+  tw`font-size[.8rem] mb-1`,
+  bold && tw`font-weight[bold]`,
+  red && tw`text-red-600`,
+]);
+export const NoteContainer = tw.div`background-color[#d6d6d6] p-4 rounded-lg`;
+export const Button = tw.button`border-none flex justify-center font-bold text-white background-color[#488c6a] font-size[2rem] width[80%] py-1 rounded-lg active:background-color[#2e6b4d] mt-20`;
+
+export const Loading = styled.div`
+  ${tw`ml-4`};
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  
+  &::after {
+    content: " ";
+    display: block;
+    width: 20px;
+    height: 20px;
+    margin: 8px;
+    border-radius: 50%;
+    border: 3px solid #fff;
+    border-color: #fff transparent #fff transparent;
+    animation: lds-dual-ring 1.2s linear infinite;
+  }
+
+  @keyframes lds-dual-ring {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 `;
